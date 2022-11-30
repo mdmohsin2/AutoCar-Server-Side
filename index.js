efce958fccfa2146aed19d469bcfe22d99b5e2a9
mdmohsin2 <mohsinbd862554@gmail.com>
2022-11-30 16:21:01 +0600

    payment completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
 
 
 const port = process.env.PORT || 5000;
@@ -26,6 +28,7 @@ async function run() {
         const allProductCollection = client.db('assignment12').collection('allProducts')
         const bookingsCollection = client.db('assignment12').collection('bookingProducts')
         const usersCollection = client.db('assignment12').collection('users')
+        const paymentsCollection = client.db('assignment12').collection('payment')
 
         // home categories setup  
         app.get('/categories', async (req, res) => {
@@ -45,6 +48,7 @@ async function run() {
             res.send(result);
         });
 
+
         // modal booking api get setup my orders page
         app.get('/bookings', async (req, res) => {
             const email = req.query.email;
@@ -53,13 +57,68 @@ async function run() {
             res.send(cursor);
         })
 
+        // all bookings single id
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const bookings = await bookingsCollection.findOne(query);
+            res.send(bookings);
+        })
+
+
         // modal booking api setup
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
-            // const query = {}
+            const query = {bookingId: booking.bookingId}
+            const alreadyBook = await bookingsCollection.find(query).toArray();
+            if(alreadyBook.length){
+                const message = 'Already have been booked this product';
+                return res.send({acknowledged:false, message})
+            }
             const result = await bookingsCollection.insertOne(booking);
             res.send(result)
         });
+
+
+        // create payments intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
+        // payments all
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            console.log(payment);
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const option = {upsert:true}
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    // transactionId: payment.transactionId
+                    isAdvertise:false,
+                    status:'sold'
+                }
+            }
+            const updatedResult = await allProductCollection.updateOne(filter, updatedDoc,option)
+            res.send(result);
+        })
+
 
 
         // my product api setup 
@@ -85,6 +144,7 @@ async function run() {
             res.send(result)
         });
 
+
         // All sellers
         app.get('/allSellers', async (req, res) => {
             const query = { accountType: 'Seller' }
@@ -92,6 +152,7 @@ async function run() {
             res.send(result)
 
         });
+
 
         // All Buyer
         app.get('/allBuyer', async (req, res) => {
@@ -101,12 +162,21 @@ async function run() {
 
         });
 
+
         // all users set
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const email = user.email;
+            const query = {email: email};
+            const alreadyUser = await usersCollection.find(query).toArray();
+            if(alreadyUser.length){
+                const message = `Already have an this account`;
+                return res.send({acknowledged: false, message});
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result)
         });
+
 
         // single main Buyer get
         app.get('/users/Buyer/:email', async (req, res) => {
@@ -174,21 +244,22 @@ async function run() {
             res.send(result);
         });
 
+
         // advertise get
-        app.get('/advertise', async(req,res)=>{
-            const query = {isAdvertise: true}
+        app.get('/advertise', async (req, res) => {
+            const query = { isAdvertise: true }
             const result = await allProductCollection.find(query).toArray();
             res.send(result)
         })
+
 
         // single main admin get
         app.get('/users/admin/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email }
             const user = await usersCollection.findOne(query);
-            res.send({ isAdmin: user?.role === 'admin' });
-        })
-
+            res.send({ isAdmin: user?.accountType === 'admin' });
+        });
 
 
     }
